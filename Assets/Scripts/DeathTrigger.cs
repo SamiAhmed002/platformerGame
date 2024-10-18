3d80c789a40a811341f7d95793600621d3157d7a
Hafsah Javed <hafsahj1@hotmail.co.uk>
2024-10-18 14:46:00 +0100

    project

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathTrigger : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 initialPosition; 
+
+    void Start()
+    {
+        // Track spawn position to teleport player on collision
+        initialPosition = player.transform.position;
+        Debug.Log(initialPosition);
+    }
+
+    // Detect when the player collides with enemy
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the colliding object is the player and not another object
+        if (other.gameObject == player)
+        {
+            // Teleport player back to spawn
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
